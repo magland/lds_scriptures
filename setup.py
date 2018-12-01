11f3d2248f382358c9c5b365a8ab5124db105eed
Jeremy Magland <jeremy.magland@gmail.com>
2018-12-01 00:26:54 -0500

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="lds_scriptures",
-    version="0.1.0",
+    version="0.1.1",
     author="Jeremy Magland",
     author_email="jeremy.magland@gmail.com",
     description="LDS Scripture Data",
